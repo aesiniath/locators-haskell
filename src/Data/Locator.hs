47152b8b2d509a7f36ecc7eca65697fec965efd9
Andrew Cowie <andrew@operationaldynamics.com>
2014-01-12 12:35:51 +1100

    Initial import code
    These sources derived from the base62 code originally published in
    oprdyn/redirector. Relicenced BSD3 from GPLv2 2 Jan 2014, and now
    released as an independent library.

---
+++ src/Data/Locator.hs
@@ -0,0 +1,25 @@
+--
+-- Human exchangable identifiers and locators
+--
+-- Copyright © 2011-2014 Operational Dynamics Consulting, Pty Ltd
+--
+-- The code in this file, and the program it is a part of, is
+-- made available to you by its authors as open source software:
+-- you can redistribute it and/or modify it under the terms of
+-- the BSD licence.
+--
+-- This code originally licenced GPLv2. Relicenced BSD3 on 2 Jan 2014.
+--
+
+{-# LANGUAGE OverloadedStrings #-}
+
+module Data.Locator
+(
+    fromLocator16,
+    toLocator16,
+    toLocator16a,
+    hashStringToLocator16a
+) where
+
+import Data.Locator.Hashes
+import Data.Locator.Locators
---
+++ tests/snippet.hs
@@ -0,0 +1,51 @@
+--
+-- Human exchangable identifiers and locators
+--
+-- Copyright © 2013-2014 Operational Dynamics Consulting, Pty Ltd and Others
+--
+-- The code in this file, and the program it is a part of, is
+-- made available to you by its authors as open source software:
+-- you can redistribute it and/or modify it under the terms of
+-- the BSD licence.
+--
+
+{-# LANGUAGE OverloadedStrings        #-}
+{-# OPTIONS -fno-warn-unused-imports #-}
+
+module Main where
+
+--
+-- Otherwise redundent imports, but useful for testing in GHCi.
+--
+
+import Prelude hiding (max)
+
+import Data.ByteString (ByteString)
+import qualified Data.ByteString as S
+import qualified Data.ByteString.Lazy as L
+import Data.Int (Int64)
+import Data.Map (Map)
+import qualified Data.Map.Strict as Map
+import Data.Serialize
+import Data.Text (Text)
+import qualified Data.Text as T
+import Data.Text.Encoding
+import Data.Word (Word8, Word32, Word64)
+import Debug.Trace
+
+--
+-- What we're testing
+--
+
+
+import Data.Locators
+
+main = do
+    let num = 0xFFFFFFFFFFFFFFF
+    putStrLn $ toLocator16  num
+    putStrLn $ toLocator16a num
+
+    putStrLn ""
+    putStrLn $ show $ num
+    putStrLn $ show $ fromLocator16 $ toLocator16  num
+
